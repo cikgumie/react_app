31fd9fc309159c7c44404b6912d5073caaaff04c
helmi <cikguhelmi83@gmai.com>
2024-11-26 23:12:44 +0800

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_app/"  // tambah tanda titik dua
-})
+  base: "/react_app/", // YOUR REPO NAME HERE
+});
